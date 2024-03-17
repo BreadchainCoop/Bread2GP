67b18d57b256028d7a04e8490425a92d9a3ebb4a
Ron Turetzky <turetzkyron@gmail.com>
2024-03-17 03:27:22 +0000

    pushing for deploy attempt

--- script/Bread2GnosisPay.s.sol
+++ script/Bread2GnosisPay.s.sol
@@ -3,7 +3,11 @@ pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
 import {Bread2GnosisPay} from "../src/Bread2GnosisPay.sol";
+import {TransparentUpgradeableProxy} from "openzeppelin-contracts/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+
 contract Bread2GPayScript is Script {
+    Bread2GnosisPay public bread2gnosispay;
+
     function setUp() public {
         Bread2GnosisPay bread2gnosispayimplementation = new Bread2GnosisPay();
         bread2gnosispay =  Bread2GnosisPay(
